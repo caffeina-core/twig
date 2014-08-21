51d6e3a52bd84abba17e4da3a4aea1d2ea8b0ae3
Stefano Azzolini <lastguest@gmail.com>
2014-08-21 11:48:23 +0200

    Create bundle.php

---
+++ bundle.php
@@ -0,0 +1,8 @@
+<?php
+return [
+    'info' => [
+        'name'          => 'Twig Templating System',
+        'url'           => 'http://caffeina.co/tech/core',
+        'description'   => 'Twig Bundle (c) Caffeina 2014 - All Rights Reserved',
+    ],
+];
